1ad4282419cf318cd6b0a4d0ccb024153df837bc
NidhalKhalfallah <48857815+NidhalKhalfallah@users.noreply.github.com>
2022-10-08 17:24:08 +0200

    Update TestFile.py

--- TestFile.py
+++ TestFile.py
@@ -2,17 +2,15 @@ from flask import Flask, request, redirect
 import os
 import subprocess
 import sys
-import MySQLdb
 
 app = Flask(__name__)
 
 
-def get_db_connection():
-        try:
-        conn = MySQLdb.connect(host=os.environ['DB_HOST'],
-                               user=os.environ['DB_USER'],
-                               passwd=os.environ['DB_PASSWORD'],
-                               db=os.environ['DB_NAME'])
-        return conn
+
+def ping():
+    url = request.args.get('url')
+    output = subprocess.Popen(["/usr/bin/ping", "-c 1", url], stdout=subprocess.PIPE).communicate()[0]
+    return output
+
 
 
