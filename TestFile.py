718c547b1bf1fa27af64006598e60d7c79f56f12
NidhalKhalfallah <48857815+NidhalKhalfallah@users.noreply.github.com>
2022-10-08 17:20:01 +0200

    Update TestFile.py

--- TestFile.py
+++ TestFile.py
@@ -1,1 +1,18 @@
-print(“Hello World”)
+from flask import Flask, request, redirect
+import os
+import subprocess
+import sys
+import MySQLdb
+
+app = Flask(__name__)
+
+
+def get_db_connection():
+        try:
+        conn = MySQLdb.connect(host=os.environ['DB_HOST'],
+                               user=os.environ['DB_USER'],
+                               passwd=os.environ['DB_PASSWORD'],
+                               db=os.environ['DB_NAME'])
+        return conn
+
+
