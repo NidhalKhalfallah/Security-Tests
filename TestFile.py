6f48a080216f2bb104e56c0671f626ec1e530612
NidhalKhalfallah <48857815+NidhalKhalfallah@users.noreply.github.com>
2022-10-09 16:41:45 +0200

    Update TestFile.py

--- TestFile.py
+++ TestFile.py
@@ -1,19 +1,6 @@
-from django.conf.urls import url
-from django.db import connection
-
-
-def show_user(request, username):
-    with connection.cursor() as cursor:
-        # BAD -- Using string formatting
-        cursor.execute("SELECT * FROM users WHERE username = '%s'" % username)
-        user = cursor.fetchone()
-
-        # GOOD -- Using parameters
-        cursor.execute("SELECT * FROM users WHERE username = %s", username)
-        user = cursor.fetchone()
-
-        # BAD -- Manually quoting placeholder (%s)
-        cursor.execute("SELECT * FROM users WHERE username = '%s'", username)
-        user = cursor.fetchone()
-
-urlpatterns = [url(r'^users/(?P<username>[^/]+)$', show_user)]
+ages = {
+    'pam': 24,
+    'jim': 24
+    'michael': 43
+}
+print(f'Michael is {ages["michael"]} years old.')
