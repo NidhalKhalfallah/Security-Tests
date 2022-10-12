0ea02e30dc3ecbccfdbae3764b8d9ff92ddf4c51
NidhalKhalfallah <48857815+NidhalKhalfallah@users.noreply.github.com>
2022-10-12 19:02:24 +0200

    Update TestFile.py

--- TestFile.py
+++ TestFile.py
@@ -1,6 +1,10 @@
-ages = {
-    'pam': 24,
-    'jim': 24
-    'michael': 43
-}
-print(f'Michael is {ages["michael"]} years old.')
+from numpy import ndarray
+
+#fill array
+a = ndarray((5,),int)
+a[0] = 1
+a[1] = 2
+a[2] = 3
+a[3] = 4
+a[4] = 5
+a[5] = 6
